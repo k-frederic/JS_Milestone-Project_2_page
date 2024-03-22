f5ceb08ff7db9ca526b34aaeb31e6b4d0f064ffd
k-frederic <thanxname@outlook.com>
2024-03-22 15:16:51 +0000

    feat: Added google Map on index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 
 
    
-  
+   let map;
+
+   function initMap() {
+    // const { Map } = await google.maps.importLibrary("maps");
+   
+     map = new google.maps.Map(document.getElementById("map"), {
+       center: { lat: 52.1148117, lng: -0.4490071 },
+       zoom: 12,
+     });
+   }
+   
+   window.initMap= initMap;
    
